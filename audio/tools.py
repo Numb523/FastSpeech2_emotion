636d475700f2ba8c48e7aa1570101f582d8e3563
April Zhou <2240162481@qq.com>
2022-03-24 09:57:10 +0800

    解决"No module named clip"：clip->clamp

--- audio/tools.py
+++ audio/tools.py
@@ -6,7 +6,7 @@ from audio.audio_processing import griffin_lim
 
 
 def get_mel_from_wav(audio, _stft):
-    audio = torch.clip(torch.FloatTensor(audio).unsqueeze(0), -1, 1)
+    audio = torch.clamp(torch.FloatTensor(audio).unsqueeze(0), -1, 1)
     audio = torch.autograd.Variable(audio, requires_grad=False)
     melspec, energy = _stft.mel_spectrogram(audio)
     melspec = torch.squeeze(melspec, 0).numpy().astype(np.float32)
